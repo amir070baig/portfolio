13009b9e5da842e15d6f275afd6dadae9fa53340
AmirTechDev <amirbaig070@gmail.com>
2025-08-21 14:26:32 +0530

    Added Projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -17,4 +17,37 @@ export const projects = [
     liveUrl: 'https://tourbooking-two.vercel.app/',
     githubUrl: 'https://github.com/amir070baig/tourbooking',
   },
+  {
+    title: 'AI Resume Builder',
+    description:
+      "An AI-powered resume builder that generates professional, ATS-friendly resumes in seconds.",
+    stack: ['Next.js', 'React', 'OpenAI', 'Tailwind CSS', 'TypeScript', 'Vercel'],
+    liveUrl: 'https://ai-resume-builder-gamma-opal.vercel.app/',
+    githubUrl: 'https://github.com/amir070baig/ai-resume-builder',
+  },
+  {
+    title: 'CartSwift E-commerce',
+    description:
+      "A polished mini e-commerce store built in 1 day to demonstrate solid full-stack skills.",
+    stack: ['Next.js', 'Express.js', 'Prisma ORM', 'React', 'Tailwind CSS', 'TypeScript'],
+    liveUrl: 'Live URL coming soon',
+    githubUrl: 'https://github.com/amir070baig/cartswift-frontend',
+  },
+  {
+    title: 'Task Tracker',
+    description:
+      "A full-stack Task Tracker application to manage daily tasks with authentication, CRUD operations, and a clean UI.",
+    stack: ['Next.js', 'Express.js', 'Prisma ORM', 'React', 'Tailwind CSS', 'TypeScript'],
+    liveUrl: 'Live URL coming soon',
+    githubUrl: 'https://github.com/amir070baig/task-tracker',
+  },
+  {
+    title: 'Auth Blog',
+    description:
+      "A full-stack Task Tracker application to manage daily tasks with authentication, CRUD operations, and a clean UI.",
+    stack: ['Next.js', 'Express.js', 'Prisma ORM', 'React', 'Tailwind CSS', 'TypeScript'],
+    liveUrl: 'Live URL coming soon',
+    githubUrl: 'https://github.com/amir070baig/auth-blog',
+  },
+
 ];
